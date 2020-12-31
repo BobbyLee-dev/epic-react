e8d08c0576fc351314e780c93e833bcd0e9303cc
Bobby Lee <rdlee8181@gmail.com>
2020-12-31 14:23:47 -0800

    Feat: Form validation

--- react-fundamentals/src/exercise/06.js
+++ react-fundamentals/src/exercise/06.js
@@ -4,54 +4,42 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
-
-  // 🐨 add the onSubmit handler to the <form> below
-
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
   const inputEl = React.useRef(null)
-  const [errorState, setErrorState] = React.useState(null)
+  // const [inputValue, setInputValue] = React.useState('')
+  const [error, setError] = React.useState(null)
 
-  // const handleSubmit = event => {
-  //   event.preventDefault()
-  //   console.log(inputEl.current.value)
-  //   // onSubmitUsername(inputEl.current.value)
-  // }
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log()
+    onSubmitUsername(inputEl.current.value)
+  }
 
   const handleChange = event => {
-    // console.log(event.target.value)
-    const inputValue = event.target.value
-    console.log(inputValue)
-    // if (inputValue !== inputValue.toLowerCase()) {
-    //   setErrorState = 'Username must be lower case'
-    //   // disable submit button
-    //   // display error in an element
-    // } else {
-    //   setErrorState = null
-    // }
+    const {value} = event.target
+    const isLowerCase = value === value.toLowerCase()
+    setError(
+      isLowerCase
+        ? null
+        : `Username must be lowercase, ${value} contains uppercase characters.`,
+    )
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="userInput">Username:</label>
         <input
           onChange={handleChange}
           ref={inputEl}
           id="userInput"
+          name="userInput"
           type="text"
         />
+        {error && <div style={{color: 'red'}}>{error}</div>}
       </div>
-      <button type="submit">Submit</button>
+      <button disabled={Boolean(error)} type="submit">
+        Submit
+      </button>
     </form>
   )
 }
